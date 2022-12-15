4b15f2418d8b827e9064d39bc0a7aa8adbb2d059
Margrietj <margrietjellema32@gmail.com>
2022-12-15 15:51:23 +0100

    navbar

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,18 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img class="logo" src="@/assets/images/Doedactiek-logo.png" alt="Doedactiek logo">
-
-    <div class="wrapper">
-      <nav class="primary-nav">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+  <header class="primary-header">
+    <div class="container">
+      <div class="nav-wrapper">
+        <img class="logo" src="@/assets/images/Doedactiek-logo.png" alt="Doedactiek logo">
+        <nav class="primary-nav">
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+          <RouterLink to="/kerst">Kerst</RouterLink>
+          <RouterLink to="/oud-en-nieuw">Oud en nieuw</RouterLink>
+        </nav>
         <RouterLink to="/kerst">Kerst</RouterLink>
-        <RouterLink to="/oud-en-nieuw">Oud en nieuw</RouterLink>
-      </nav>
+      </div>
     </div>
   </header>
 
@@ -24,41 +26,24 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-  width: 100%;
-}
 
 .logo {
   display: block;
-  margin: 0 auto 2rem;
   width: 160px;
 }
 
 .primary-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  display: flex;
+  gap: 1.5em;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.nav-wrapper a {
+  text-decoration: none;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
+.nav-wrapper {
+  display: flex;
+  justify-content: space-between;
 }
 
 footer {
